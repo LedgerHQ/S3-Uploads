a84ae372279367038a327f36ea02dd62376a50c1
Louis Cathala <louis.cathala@protonmail.com>
2022-05-02 12:02:11 +0200

    feat: add php filter for sessino token

--- s3-uploads.php
+++ s3-uploads.php
@@ -12,6 +12,12 @@ require_once __DIR__ . '/inc/namespace.php';
 
 add_action( 'plugins_loaded', 'S3_Uploads\\init', 0 );
 
+add_filter( 's3_uploads_s3_client_params', function ( $params ) {
+	$params['credentials']['token'] = getenv('AWS_SESSION_TOKEN');
+	return $params;
+} );
+
+
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
 	WP_CLI::add_command( 's3-uploads', 'S3_Uploads\\WP_CLI_Command' );
 }
